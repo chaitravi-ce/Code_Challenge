53e0289fe22172ca7deac403821f7b5010403d31
chaitravi-ce <chaitravi007@gmail.com>
2021-03-31 20:35:44 +0530

    Updated Code

--- ProLib.py
+++ ProLib.py
@@ -1,14 +1,15 @@
 import numpy as np
 from PIL import Image as im
+from PIL import ImageDraw
+
 
 class ProLib:
 
-    def __init__(self,path):
+    def __init__(self, path):
         print('init')
         self.img = im.open(path)
-        self.npimg = np.array(self.img)
-        self.h, self.w = self.npimg.shape
-
+        self.npImg = np.array(self.img)
+        self.h, self.w = self.npImg.shape
 
     def median_filter(self):
         sumi = np.zeros((5, 5, 3), dtype=np.uint32)
@@ -17,88 +18,108 @@ class ProLib:
                 if i <= 5 or j <= 5 or i >= self.h - 5 or j >= self.w - 5:
                     continue
                 else:
-                    sumi = self.npimg[i - 2:i + 3, j - 2:j + 3]
+                    sumi = self.npImg[i - 2:i + 3, j - 2:j + 3]
                     sumi = np.median(sumi, axis=0)
                     sumi = np.median(sumi, axis=0)
-                    self.npimg[i, j] = sumi
-        return self.npimg
+                    self.npImg[i, j] = sumi
+        return self.npImg
 
-    def apply_kernel(self,kernel):
+    def apply_kernel(self, kernel):
         print(kernel)
-        new_img = np.zeros_like(self.npimg)
-        h, w = self.npimg.shape
+        new_img = np.zeros_like(self.npImg)
+        h, w = self.npImg.shape
         for i in range(3, h - 3):
             for j in range(3, w - 3):
-                x = np.sum(self.npimg[i:i + 3, j:j + 3] * kernel)
-                if (x < 0):
+                x = np.sum(self.npImg[i:i + 3, j:j + 3] * kernel)
+                if x < 0:
                     x = 0
-                elif (x > 255):
+                elif x > 255:
                     x = 255
-                self.npimg[i][j] = x
+                self.npImg[i][j] = x
 
-        self.npimg = self.npimg / self.npimg.max() * 255
-        return self.npimg
+        self.npImg = self.npImg / self.npImg.max() * 255
+        return self.npImg
 
     def suppress_nonmax(self, angle):
-        new_img = np.zeros_like(self.npimg)
+        new_img = np.zeros_like(self.npImg)
         for i in range(1, self.h - 1):
             for j in range(1, self.w - 1):
                 # angle 0
                 if (0 <= angle[i, j] < 22.5) or (157.5 <= angle[i, j] <= 180):
-                    q = self.npimg[i, j + 1]
-                    r = self.npimg[i, j - 1]
+                    q = self.npImg[i, j + 1]
+                    r = self.npImg[i, j - 1]
                 # angle 45
-                elif (22.5 <= angle[i, j] < 67.5):
-                    q = self.npimg[i + 1, j - 1]
-                    r = self.npimg[i - 1, j + 1]
+                elif 22.5 <= angle[i, j] < 67.5:
+                    q = self.npImg[i + 1, j - 1]
+                    r = self.npImg[i - 1, j + 1]
                 # angle 90
-                elif (67.5 <= angle[i, j] < 112.5):
-                    q = self.npimg[i + 1, j]
-                    r = self.npimg[i - 1, j]
+                elif 67.5 <= angle[i, j] < 112.5:
+                    q = self.npImg[i + 1, j]
+                    r = self.npImg[i - 1, j]
                 # angle 135
-                elif (112.5 <= angle[i, j] < 157.5):
-                    q = self.npimg[i - 1, j - 1]
-                    r = self.npimg[i + 1, j + 1]
-                if (self.npimg[i][j] >= q and self.npimg[i][j] >= q):
-                    new_img = self.npimg
+                elif 112.5 <= angle[i, j] < 157.5:
+                    q = self.npImg[i - 1, j - 1]
+                    r = self.npImg[i + 1, j + 1]
+                if self.npImg[i][j] >= q and self.npImg[i][j] >= q:
+                    new_img = self.npImg
         return new_img
 
     def threshold(self, low=0.05, high=0.09):
-        hight = self.npimg.max() * high
+        hight = self.npImg.max() * high
         lowt = hight * low
-        strongi, strongj = np.where(self.npimg >= hight)
-        weaki, weakj = np.where((self.npimg >= lowt) & (self.npimg <= hight))
-        new_img = np.zeros_like(self.npimg)
+        strongi, strongj = np.where(self.npImg >= hight)
+        weaki, weakj = np.where((self.npImg >= lowt) & (self.npImg <= hight))
+        new_img = np.zeros_like(self.npImg)
         new_img[strongi, strongj] = 255
         new_img[weaki, weakj] = 25
         kernel = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        new_img2 = np.zeros_like(self.npimg)
+        new_img2 = np.zeros_like(self.npImg)
         for i in range(3, self.h - 3):
             for j in range(3, self.w - 3):
-                if (new_img[i, j] == 25):
+                if new_img[i, j] == 25:
                     x = np.sum(new_img[i:i + 3, j:j + 3] * kernel)
-                    if (x >= 255):
-                        self.npimg[i, j] = 255
-                elif (new_img[i, j] == 255):
-                    self.npimg[i, j] = 255
-        return self.npimg
+                    if x >= 255:
+                        self.npImg[i, j] = 255
+                elif new_img[i, j] == 255:
+                    self.npImg[i, j] = 255
+        return self.npImg
 
-    def getdist(self,x, y):
-        mindist = np.sqrt(self.npimg.shape[0] ** 2 + self.npimg.shape[1] ** 2)
+    def getdist(self, x, y):
+        mindist = np.sqrt(self.npImg.shape[0] ** 2 + self.npImg.shape[1] ** 2)
         xN = 0
         yN = 0
-        print(mindist)
         for i in range(self.h):
             for j in range(self.w):
-                val = self.npimg[i, j]
-                if (val > 15):
+                val = self.npImg[i, j]
+                if val > 10:
                     dist = np.sqrt((x - i) ** 2 + (y - j) ** 2)
-                    if (dist < mindist):
+                    if dist < 3:
+                        print(dist)
+                    if dist < mindist:
                         xN = i
                         yN = j
                         mindist = dist
         print(mindist)
         print(xN)
         print(yN)
-        return (xN.yN)
+        return xN, yN
 
+    def showImg(self):
+        img = self.npImg
+        img = img.astype(np.uint8)
+        po = im.fromarray(img)
+        im._show(po)
+        return po
+
+    def saveImg(self,name):
+        img = self.showImg()
+        img.save(name)
+
+
+    def drawImg(self,x1,y1,x2,y2):
+        img = self.npImg
+        img = img.astype(np.uint8)
+        po = im.fromarray(img)
+        draw = ImageDraw.Draw(po)
+        draw.line([(x1,y1),(x2,y2)],width=2, fill= 180)
+        po.show()
--- test.py
+++ test.py
@@ -1,16 +1,37 @@
 from ProLib import *
 
 path = './brainMRI.png'
+
+# Initializing Object
 im = ProLib(path)
 print(im)
+
+#Applying Median Filter
 im.median_filter()
+
+#Smoothining
 kernel = np.array([[1 / 16, 2 / 16, 1 / 16], [1 / 8, 1 / 4, 1 / 8], [1 / 16, 2 / 16, 1 / 16]])
 im.apply_kernel(kernel)
+
+#Applying Vertical Edge Detection Filter
 kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
 imgy = im.apply_kernel(kernel)
+
+#Applying Horizontal Edge Detection Filter
 kernel = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
 imgx = im.apply_kernel(kernel)
-im.npimg = np.sqrt(np.square(imgx) + np.square(imgy))
-img = im.npimg.astype(np.uint8)
-po = img.fromarray(img)
 
+#Sobel
+im.npImg = np.sqrt(np.square(imgx) + np.square(imgy))
+
+#Canny
+theta=np.arctan2(imgy,imgx)
+im.suppress_nonmax(theta)
+im.threshold()
+
+
+im.showImg()
+im.saveImg("canny.jpg")
+x1, y1 = 50, 50
+x2, y2 = im.getdist(x1, y1)
+#im.drawImg(x1,y1,x2,y2)
